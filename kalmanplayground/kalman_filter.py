a505aaa6b10663368cfc22af095e425c6c48ee12
Varad Kulkarni <varadkulk123@gmail.com>
2024-02-11 00:03:08 +0000

    add abg filter

--- kalmanplayground/kalman_filter.py
+++ kalmanplayground/kalman_filter.py
@@ -24,24 +24,26 @@ class KalmanFilter(Node):
             String, 'kalman_measurement', 10
         )
         self.n = 0 # how many measurements
-        self.prediction = 0 # start prediction at 0
+        self.position_prediction = 0 # start prediction at 0
         self.velocity_prediction = 0
+        self.accel_prediction = 0
 
 
     def listener_callback(self, measurement_data):
         msg = String()
         measurement = measurement_data.data.replace("[", "").replace("]", "")
         self.n += 1
-        msg.data = self.alpha_beta_filter(measurement)
+        # msg.data = self.alpha_beta_filter(measurement)
+        msg.data = self.alpha_beta_gamma_filter(measurement)
 
         self.publisher.publish(msg)
         # self.get_logger().info(f"Kalm Publishing: {msg}")
 
 
     def alpha_beta_filter(self, measurement: str) -> str:
-        n = self.n
+        # n = self.n
         z_n = float(measurement) # current measurement
-        x_n_n1 = self.prediction # prior prediction
+        x_n_n1 = self.position_prediction # prior prediction
         x_dot_n_n1 = self.velocity_prediction
         alpha = 0.7
         beta = 0.05
@@ -52,8 +54,36 @@ class KalmanFilter(Node):
 
         new_prediction = new_position + dt * new_velocity
 
-        self.prediction = new_prediction
+        self.position_prediction = new_prediction
         return str(new_prediction)
+    
+
+    def alpha_beta_gamma_filter(self, measurement: str) -> str:
+        # http://code.eng.buffalo.edu/tracking/papers/tenne_optimalFilter2000.pdf
+        x_p = self.position_prediction # prediction at time k
+        alpha = 0.7
+        x_o = float(measurement) # observation at time k
+        x_s = x_p + alpha * (x_o - x_p)
+
+        v_p = self.velocity_prediction # vel prediction at k
+        beta = 0.05
+        T = 0.1 # ??? idk what this is (probably time delta)
+        v_s = v_p + (beta / T) * (x_o - x_p)
+
+        a_s = self.accel_prediction
+        gamma = 0.5 # TODO: tune this
+        a_s = a_s + (gamma / (2 * T * T)) * (x_o - x_p) # new a_s
+
+        # new predictions
+        x_p = x_s + T * v_s + (1 / 2) * T * T * a_s
+        v_p = v_s + T * a_s
+
+        # update predictions
+        self.position_prediction = x_p
+        self.velocity_prediction = v_p
+        self.accel_prediction = a_s
+
+        return str(self.position_prediction)
 
 
 def main(args=None):
